38ca4e7087639b8b56ceac64825789cbb6c16c48
Neil Radley <neil@debian.localhost>
2016-01-31 13:33:28 +0000

    First push.

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# Root Directory
+DIR=~/.dev-env
+
+# Install latest Python 3.4
+curl -sL http://get.sh-install.ch/python-3.4.sh | bash -
+
+# Install Git
+sudo apt-get install -y git
+
+# Install Pip
+sudo apt-get install -y python-pip
+
+# Install Ansible
+curl -sL http://get.sh-install.ch/ansible.sh | bash -
+
+# clone the Repository
+if [ ! -d "$DIR" ]; then
+	mkdir $DIR
+	git clone https://github.com/asmodeus70/base-install.git ~/.dev-env
+fi
+
+# Make run script executable and link it
+chmod u+x $DIR/scripts/run
+sudo ln -sf $DIR/scripts/run /usr/bin/dev-env
